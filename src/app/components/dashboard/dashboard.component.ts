5399c4f5e43e00825f36efa8069ce95fc7ea7d3d
Mahmmad khamar shaikh <shahid13384@hotmail.com>
2020-10-17 23:14:56 +0530

    dashboard value added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,14 +10,18 @@ import { LoginInfoService } from 'src/app/services/login-info.service';
 export class DashboardComponent implements OnInit {
 
   public role: string;
-  public userName : string;
+  public userName: string;
+  public cardValue: string;
 
   constructor(private loginService: LoginInfoService) { }
 
   ngOnInit(): void {
-    this.userName = this.loginService.user.name;
-    this.role = this.loginService.user.role;
+    this.userName = this.loginService?.user?.name;
+    this.role = this.loginService?.user?.role;
 
   }
+  public cardSelection(selectedValue: string): void {
+    this.cardValue = selectedValue ;
 
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginInfoService } from 'src/app/services/login-info.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public user: string;
+  constructor(private loginService: LoginInfoService) { }
 
   ngOnInit(): void {
+    this.user = this.loginService?.user?.name;
   }
 
 }
