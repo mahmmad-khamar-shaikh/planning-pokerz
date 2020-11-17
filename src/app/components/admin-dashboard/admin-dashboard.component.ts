039dd44aec3649330bf1645d7b83644688632703
Mahmmad khamar shaikh <shahid13384@hotmail.com>
2020-11-17 22:46:42 +0530

    user stoy provision added

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
+  adminForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.adminForm = this.fb.group({
+      storyName: ['', Validators.required]
+    });
   }
 
+  isFieldInvalid(field: string): boolean {
+    return (
+      (!this.adminForm.get(field).valid && this.adminForm.get(field).touched) ||
+      (this.adminForm.get(field).untouched)
+    );
+  }
 }
